02e2474359e0b4fd3a7b52cb8ebb4cda7af30dfd
Karan Swami <karanswm934@gmail.com>
2025-08-16 20:19:20 +0530

    Initial commit

---
+++ merge.js
@@ -0,0 +1,15 @@
+const PDFMerger = require('pdf-merger-js').default; 
+
+async function mergePdfs(p1, p2) {
+  const merger = new PDFMerger();
+
+  await merger.add(p1);
+  await merger.add(p2);
+
+  let d = new Date().getTime()
+  await merger.save(`public/${d}.pdf`);
+
+  return d;
+}
+
+module.exports = { mergePdfs };
